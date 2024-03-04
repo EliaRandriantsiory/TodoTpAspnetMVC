c6db5d01a0f332f4776caa73331420cea3d857f1
Farahary <faratsm@gmail.com>
2024-03-04 14:59:59 +0300

    création controller et view login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Utilisateur utilisateur)
+        {
+            return Content("Login");
+    }
+    }
+}
