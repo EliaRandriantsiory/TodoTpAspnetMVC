7cdd4d6a6a41aeb23123f6fe17b3b0a6140972d0
Farahary <faratsm@gmail.com>
2024-03-05 10:06:25 +0300

    copy du branche fara dans farahary

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Utilisateur utilisateur)
+        {
+            return Content("Login");
+        }
+
+        [HttpGet]
+        public ActionResult CreationCompte()
+        {
+            return View();
+        }
+
+    }
+}
